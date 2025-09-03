9b56f73cd6a26a9d8f1f0ffd53122212dd9e1987
yeeun <jovm268@naver.com>
2025-09-03 15:14:52 +0900

    chore : esLint,prettier 환경설정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }], // '@'를 '/src' 폴더로 대체
+  },
+});
