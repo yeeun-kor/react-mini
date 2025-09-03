2b5c413486b8c01eaa964a10dbe222d35b6adf2d
yeeun <jovm268@naver.com>
2025-09-03 16:54:12 +0900

    feat : 카드컴포넌트 Link로 Details페이지 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
+import { Link } from 'react-router-dom';
 import './App.css';
-import MovieDetail from './components/MovieDetail';
-import movieDatailData from './data/movieDetailData.json';
+import MovieCard from './components/MovieCard';
 import movieListData from './data/movieListData.json';
 
 //상수
 export const BASE_IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const movies = movieListData.results;
-const detail = movieDatailData;
 
 export //TODOS data:RTK로 관리하기
 function App() {
-  //json 데이터 가져오기
-
-  console.log(detail);
   return (
     <>
       <h1 className="bg-indigo-300 p-3 text-center text-3xl">메인페이지</h1>
-      {/* <main className="m-auto grid max-w-[1080px] grid-cols-1 gap-4 px-5 py-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
+      <main className="m-auto grid max-w-[1080px] grid-cols-1 gap-4 px-5 py-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
         {movies.map((movie) => (
-          <MovieCard key={movie.id} movie={movie}></MovieCard>
+          <Link key={movie.id} to={'/details'}>
+            <MovieCard key={movie.id} movie={movie}></MovieCard>
+          </Link>
         ))}
-      </main> */}
-      <MovieDetail detail={detail}></MovieDetail>
+      </main>
+      <></>,
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import App from './App';
+import MovieDetail from './components/MovieDetail';
+import movieDatailData from './data/movieDetailData.json';
+import './index.css';
+
+const detail = movieDatailData;
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App></App>}></Route>
+      <Route
+        path="/details"
+        element={<MovieDetail detail={detail}></MovieDetail>}
+      ></Route>
+    </Routes>
+  </BrowserRouter>,
+);
