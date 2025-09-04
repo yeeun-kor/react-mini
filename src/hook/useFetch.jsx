e5fe7eae8993adf15f5ad4300de2b49e1cd8917e
yeeun <jovm268@naver.com>
2025-09-04 18:12:42 +0900

    feat : API응답 커스텀 훅 생성

---
+++ src/hook/useFetch.jsx
@@ -0,0 +1,29 @@
+import { API_KEY } from '@/App';
+import { useEffect, useState } from 'react';
+
+export const useFetch = (url) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const options = {
+          method: 'GET',
+          headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${API_KEY}`,
+          },
+        };
+        const res = await fetch(url, options);
+        const json = await res.json();
+        console.log(json);
+        //results사용 안하고!!! 그냥 객체 전체 받아와라 !! 재사용성 높이게
+        setData(json);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [url]);
+  return data;
+};
