6e11fec2dd3c8434bd2f8a4102a7311ca441e838
yeeun <jovm268@naver.com>
2025-09-08 18:16:14 +0900

    feat : useDebounce커스텀훅

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,50 +1,35 @@
-import { debounce } from 'lodash';
-import { useEffect, useMemo, useState } from 'react';
+import useDebounce from '@/hook/useDebounce';
+import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 export const SearchBar = () => {
-  //상태관리
-  //검색어 입력
   const navigate = useNavigate();
   const location = useLocation();
+
+  //검색어 관리
   const [inputValue, setInputValue] = useState('');
-  const [debouncedVal, setDebouncedVal] = useState('');
-  console.log(inputValue);
+  //useDebounce로 검색어 업데이트
+  const debouncedVal = useDebounce(inputValue, 500);
 
-  //디바운스된 값만 inputValue로 세팅하기
-  //디바운스 함수 생성
-  //3초동안 추가 호출이 없으면, 마지막으로 전달된 값으로 함수값 실행
-  const debouncedSendRequest = useMemo(() => {
-    return debounce((inputValue) => setDebouncedVal(inputValue), 1000);
-  }, []);
-
-  //이렇게해서 상태변화된 값을 navigate로 보내기
-  const onChange = (e) => {
-    const val = e.target.value;
-    setInputValue(val); //입력한 값 화면에 반영
-    debouncedSendRequest(val); //입력된 값 디바운스로 보내기
-  };
-
-  //검색어 입력값 API로 보내기
+  //검색어 변경시 navigate로 이동
   useEffect(() => {
-    if (debouncedVal.trim()) {
+    if (debouncedVal?.trim()) {
       navigate(`/search?query=${debouncedVal}`);
     }
   }, [debouncedVal]);
 
   useEffect(() => {
-    setDebouncedVal('');
     setInputValue('');
   }, [location.pathname]);
-
   return (
     <div className="w-full flex-1 border-b">
       {' '}
       <input
         type="text"
         value={inputValue}
-        onChange={onChange}
+        onChange={(e) => setInputValue(e.target.value)}
         placeholder="영화검색하기"
+        className="focus:outline-none"
       />
     </div>
   );
