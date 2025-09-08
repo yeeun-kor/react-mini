421098ba3e165620531fec9df5faf84b9ce9562a
yeeun <jovm268@naver.com>
2025-09-08 11:59:22 +0900

    style : 반응형 텍스트 사이즈 수정

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 export default function NavBar() {
   return (
-    <nav className="w-full bg-black px-3 py-2 text-white shadow-md">
-      <div className="m-auto flex max-w-[1080px] items-center justify-between gap-4 p-4">
-        <h1 className="text-3xl font-bold">
+    <nav className="w-full bg-black px-0 py-2 text-white shadow-md md:px-3">
+      <div className="m-auto flex max-w-[1080px] flex-col items-center justify-between gap-4 p-4 md:flex-row">
+        <h1 className="text-lg font-bold md:text-3xl">
           <Link to={'/'}>🎬 OZ무비</Link>
         </h1>
         <input
@@ -13,11 +13,11 @@ export default function NavBar() {
           className="w-full flex-1 border-b"
         />
 
-        <div className="flex gap-2">
-          <button className="button rounded-full bg-indigo-500 px-3 py-1">
+        <div className="flex gap-2 md:flex-row">
+          <button className="button w-full rounded-full bg-indigo-500 px-3 py-1 text-xs md:text-lg">
             Login
           </button>
-          <button className="button rounded-full bg-indigo-500 px-3 py-1">
+          <button className="button w-full rounded-full bg-indigo-500 px-3 py-1 text-xs md:text-lg">
             JoinUs
           </button>
         </div>
