ab12e75632c74f531617818880504d6dd8c01a6e
yeeun <jovm268@naver.com>
2025-09-10 00:47:15 +0900

    docs : 리드미파일 초기화

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,24 @@
 import { useAuth } from '@/supabase/authUser';
+import { useSupabase } from '@/supabase/client';
+import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
+import { UserCircleIcon } from '@heroicons/react/16/solid';
 import { Link } from 'react-router-dom';
 import { SearchBar } from './SearchBar';
 
 export default function NavBar() {
-  const { user: user.user_metadata, setUser } = useAuth();
-  console.log(user);
+  //로그아웃 기능 구현
+  const supabase = useSupabase();
+  const { user } = useAuth();
+  // 유저이름 확인
+  const userMetaData = user?.user_metadata;
+  console.log(userMetaData);
+
+  //로그아웃 버튼
+  const handlerSubmit = async () => {
+    await supabase.auth.signOut();
+    window.location.reload();
+  };
+
   return (
     <nav className="w-full bg-black px-0 py-2 text-white shadow-md md:px-3">
       <div className="m-auto flex max-w-[1080px] flex-col items-center justify-between gap-4 p-4 md:flex-row">
@@ -14,16 +28,44 @@ export default function NavBar() {
           </Link>
         </h1>
         <SearchBar></SearchBar>
-        <div className="flex gap-2 md:flex-row">
+        <div className="flex items-center gap-2 md:flex-row">
           {user ? (
             <>
-              <span className="text-sm">안녕하세요, {user.name}님</span>
-              <button
-                onClick={() => setUser(null)}
-                className="button rounded-full bg-red-500 px-3 py-1 text-xs md:text-lg"
-              >
-                Logout
-              </button>
+              <div className="text-sm">
+                안녕하세요, {userMetaData.full_name}님
+              </div>
+              <Menu as="div" className="relative ml-3">
+                <MenuButton className="relative flex rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+                  <span className="absolute -inset-1.5" />
+                  <span className="sr-only">Open user menu</span>
+                  <UserCircleIcon
+                    aria-hidden="true"
+                    className="size-12 text-gray-300"
+                  />
+                </MenuButton>
+
+                <MenuItems
+                  transition
+                  className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg outline outline-black/5 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+                >
+                  <MenuItem>
+                    <a
+                      href="#"
+                      className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden"
+                    >
+                      관심목록
+                    </a>
+                  </MenuItem>
+                  <MenuItem>
+                    <button
+                      onClick={handlerSubmit}
+                      className="block w-full px-4 py-2 text-left text-sm text-gray-700 data-focus:bg-gray-100"
+                    >
+                      로그아웃
+                    </button>
+                  </MenuItem>
+                </MenuItems>
+              </Menu>
             </>
           ) : (
             <>
