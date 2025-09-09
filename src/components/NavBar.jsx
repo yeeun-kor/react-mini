2a7912de99f0bd425a71d78004aaaa161b415cb1
yeeun <jovm268@naver.com>
2025-09-09 23:26:54 +0900

    feat : 세션에 저장된 로그인 여부에 따라, 네브바 기능 구현

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,44 @@
-import { useState } from 'react';
+import { useAuth } from '@/supabase/authUser';
 import { Link } from 'react-router-dom';
 import { SearchBar } from './SearchBar';
 
 export default function NavBar() {
-
-
+  const { user: user.user_metadata, setUser } = useAuth();
+  console.log(user);
   return (
     <nav className="w-full bg-black px-0 py-2 text-white shadow-md md:px-3">
       <div className="m-auto flex max-w-[1080px] flex-col items-center justify-between gap-4 p-4 md:flex-row">
         <h1 className="text-lg font-bold md:text-3xl">
-          <Link to={'/'}>🎬 OZ무비</Link>
+          <Link to={'/'} className="flex items-center justify-center">
+            <img src="/src/assets/logo.png" alt="" className="h-20" /> OZ무비
+          </Link>
         </h1>
         <SearchBar></SearchBar>
         <div className="flex gap-2 md:flex-row">
-          <button className="button w-full rounded-full bg-indigo-500 px-3 py-1 text-xs md:text-lg">
-            Login
-          </button>
-          <button className="button w-full rounded-full bg-indigo-500 px-3 py-1 text-xs md:text-lg">
-            JoinUs
-          </button>
+          {user ? (
+            <>
+              <span className="text-sm">안녕하세요, {user.name}님</span>
+              <button
+                onClick={() => setUser(null)}
+                className="button rounded-full bg-red-500 px-3 py-1 text-xs md:text-lg"
+              >
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <Link to={'/login'}>
+                <button className="button w-full rounded-full bg-indigo-500 px-3 py-1 text-xs md:text-lg">
+                  Login
+                </button>
+              </Link>
+              <Link to={'/signup'}>
+                <button className="button w-full rounded-full bg-indigo-500 px-3 py-1 text-xs md:text-lg">
+                  SignUp
+                </button>
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </nav>
