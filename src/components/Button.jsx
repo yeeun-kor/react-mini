9a03798fdeb63bfbaae76357c3d528117b39f49d
yeeun <jovm268@naver.com>
2025-09-09 23:26:14 +0900

    feat : 입력폼, 버튼 컴포넌트 생성

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+export default function Button({ text }) {
+  return (
+    <button
+      type="submit"
+      className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:shadow-none dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500"
+    >
+      {text}
+    </button>
+  );
+}
