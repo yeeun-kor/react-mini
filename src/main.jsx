739245842dc242fbf2c3d495180ef9e55e1ea212
yeeun <jovm268@naver.com>
2025-09-10 13:03:23 +0900

    refactor : lazy로 번들크기 줄임

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Layout from './components/Layout';
-import MovieDetail from './components/MovieDetail';
 import './index.css';
 import Login from './pages/Login';
 import { Search } from './pages/Search';
 import { SignUp } from './pages/SignUp';
 import { AuthProvider } from './supabase/authUser';
 import { SupabaseProvider } from './supabase/client';
+const MovieDetail = React.lazy(() => import('./components/MovieDetail'));
 
 createRoot(document.getElementById('root')).render(
   <SupabaseProvider>
