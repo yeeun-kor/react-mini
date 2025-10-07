dd87f383f49a5d99c599d45667cf9647a6cca74a
yeeun <jovm268@naver.com>
2025-10-07 22:42:31 +0900

    feat : lazy,Suspense 로딩페이지 기능 추가

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,9 @@
+import Spinner from '../assets/spin.gif';
 export default function Loading() {
   return (
     <div className="absolute top-0 left-0 flex h-full w-full flex-col items-center justify-center">
-      <div className="text-center text-3xl">🌀Loading</div>
+      <div className="text-center text-3xl">Loading</div>
+      <img src={Spinner} alt="loading" width="5%" />
     </div>
   );
 }
